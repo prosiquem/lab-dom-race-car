1417ece108ea63cf012f00d83b01081614bebc84
Pedro Rosique <rosiquemurciapedro@gmail.com>
2024-10-27 19:46:39 +0100

    half solved, car not showing

--- js/game.js
+++ js/game.js
@@ -1,3 +1,50 @@
 class Game {
     // code to be added
+    constructor() {
+        this.startScreen = document.getElementById("game-intro")
+
+        this.gameScreen = document.getElementById("game-screen")
+
+        this.gameEndScreen = document.getElementById("game-end")
+
+        this.player = null
+
+        this.height = 600
+        this.width = 500
+
+        this.obstacles = []
+        this.score = 0
+        this.lives = 3
+
+        this.gameIsOver = false
+        this.gameIntervalId
+        this.gameLoopFrequency = Math.round(1000 / 60)
+    }
+
+    start() {
+
+        this.gameScreen.style.height = `${this.height}px`
+        this.gameScreen.style.width = `${this.width}px`
+
+        this.startScreen.style.display = "none"
+
+        this.gameScreen.style.display = "block"
+
+        this.gameIntervalId = setInterval(() => {
+            this.gameLoop()
+        }, this.gameLoopFrequency)
+    }
+
+    gameLoop() {
+        console.log("in the game loop")
+
+        this.update()
+
+        if (this.gameIsOver) {
+            clearInterval(this.gameIntervalId)
+        }
+    }
+
+    update() {
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
 window.onload = function () {
-  const startButton = document.getElementById("start-button");
-  const restartButton = document.getElementById("restart-button");
+  const startButton = document.getElementById("start-button")
+  const restartButton = document.getElementById("restart-button")
+  let game
 
   startButton.addEventListener("click", function () {
-    startGame();
-  });
+    startGame()
+  })
 
   function startGame() {
-    console.log("start game");
+    console.log("start game")
+    game = new Game()
+
+    game.start()
   }
-};
+
+  const keys = {
+    LEFT: ArrowLeft,
+    UP: ArrowUp,
+    RIGHT: ArrowRight,
+    DOWN: ArrowDown,
+  }
+
+  document.onkeydown = function (event) {
+    switch (event.code) {
+      case this.keys.LEFT:
+        game.player.directionX = -1
+        break
+      case this.keys.RIGHT:
+        game.player.directionX = 1
+        break
+      case this.keys.UP:
+        game.player.directionY = -1
+        break
+      case this.keys.DOWN:
+        game.player.directionY = 1
+        break
+    }
+  }
+}
